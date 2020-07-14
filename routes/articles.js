a1ff2741321fb5d96a71bdcd1653edefe70ab3c7
SafoineBouida <safoinebouida91@gmail.com>
2020-07-14 16:17:38 +0100

    File upload

--- routes/articles.js
+++ routes/articles.js
@@ -2,19 +2,50 @@ var express = require('express');
 var router = express.Router();
 var Article = require("./../models/article");
 const { request } = require('express');
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    filename = file.originalname.split('.')[0] + '-' + Date.now() + '.' + file.originalname.split('.')[1]
+    cb(null, filename)
+  }
+})
+
+var upload = multer({ storage: storage })
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post("/article", async (request, response) => {
+router.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  res.send(file)
+
+})
+
+
+router.post("/article", upload.single('article'), async (request, response) => {
   try {
     var { titre, article, user } = request.body
+    const file = request.file
+    if (file) {
+      article = file.filename
+
+    }
     obj = { titre: titre, article: article, user: user }
     article = new Article(obj);
-    await article.save()
+    data = await article.save()
     return response.status(200).send(data)
   } catch (error) {
+    console.log(error.message)
     return response.status(405).send('erreur')
   }
 });
@@ -60,13 +91,13 @@ router.get("/nbavis", async (request, response) => {
     var { article, type } = request.body
     article = await Article.findOne({ _id: article }).exec();
     if (article) {
-      if(type){
-        var count = await article.avis.filter( x => x.type).length
+      if (type) {
+        var count = await article.avis.filter(x => x.type).length
       } else {
-        var count = await article.avis.filter( x => !x.type).length
-      } 
+        var count = await article.avis.filter(x => !x.type).length
+      }
       console.log(count)
-      return response.status(200).send({count})
+      return response.status(200).send({ count })
     }
   } catch (error) {
     return response.status(405).send('erreur')
